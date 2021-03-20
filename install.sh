617c7beb33033a1368e55190f60edc27f9515425
Cameron Clough <cameronjclough@gmail.com>
2021-03-20 12:39:43 +0000

    Update install script for rosetta-dogecoin

--- install.sh
+++ install.sh
@@ -20,7 +20,7 @@ set -e
 usage() {
   this=$1
   cat <<EOF
-$this: download pre-compiled Docker images for coinbase/rosetta-bitcoin 
+$this: download pre-compiled Docker images for rosetta-dogecoin/rosetta-dogecoin
 
 Usage: $this [-d]
   -d turns on debug logging
@@ -44,8 +44,8 @@ execute() {
   log_info "downloading image into ${tmpdir}"
   http_download "${tmpdir}/${TARBALL}" "${TARBALL_URL}" "" "1"
   docker load --input "${tmpdir}/${TARBALL}"
-  docker tag "rosetta-bitcoin:${TAG}" "rosetta-bitcoin:latest"
-  log_info "loaded rosetta-bitcoin:${TAG} and tagged as rosetta-bitcoin:latest"
+  docker tag "rosetta-dogecoin:${TAG}" "rosetta-dogecoin:latest"
+  log_info "loaded rosetta-dogecoin:${TAG} and tagged as rosetta-dogecoin:latest"
   rm -rf "${tmpdir}"
   log_info "removed temporary directory ${tmpdir}"
 }
@@ -196,10 +196,10 @@ End of functions from https://github.com/client9/shlib
 ------------------------------------------------------------------------
 EOF
 
-BINARY=rosetta-bitcoin
+BINARY=rosetta-dogecoin
 FORMAT=tar.gz
-OWNER=coinbase
-REPO="rosetta-bitcoin"
+OWNER=rosetta-dogecoin
+REPO=rosetta-dogecoin
 PREFIX="$OWNER/$REPO"
 
 # use in logging routines
