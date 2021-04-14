9351ca1451507a38dc9c0cb3268e51925790fbad
Patrick Lodder <patricklodder@users.noreply.github.com>
2021-04-14 17:46:30 +0200

    Fix copyright messages in changed files

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 # Copyright 2020 Coinbase, Inc.
+# Copyright 2021 Rosetta Dogecoin Developers
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
